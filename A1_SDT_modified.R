1b1e6adb4817a0d22f7af2f4c5c0dd3128e04c75
brabagaza <roelofroessingh@protonmail.com>
2018-08-11 01:29:33 +0200

    adding dynmic limit for upper limit x-values, signal_mean+ 4, should we consider adding options for negative mean values?

--- A1_SDT_modified.R
+++ A1_SDT_modified.R
@@ -3,13 +3,13 @@
 SDT <- function(criterion, signal_mean, noise_mean, input_sigma = 1, likelihood_line_col = 'gray31', col1= rgb(70/255, 130/255, 180/255, .85), col2= rgb(135/255, 206/255, 235/255, 0.35), criterion_col = "pink3", line_col = "black") {
 
   #initiate empty plot
-  plot.new()
+  3plot.new()
   #set margines:
   par(mar=c(5,5,3,7), xpd = T) #bottom, left, top and right margins, xpd = sets clipping to 'off' so that lables of likelihoods are plotted and not clipped
   
   nr_n = 50
   #plot(a)
-  curve(dnorm(x, noise_mean, input_sigma), n= nr_n, xlim=c(0, 10), ylim=c(0, .55),
+  curve(dnorm(x, noise_mean, input_sigma), n= nr_n, xlim=c(0, signal_mean+4), ylim=c(0, .55),
         xlab="Decision variable", ylab="Probability", frame=FALSE)
   legend("topright", legend=c("target", "foil"), lty=c(2, 1))
   
@@ -46,7 +46,7 @@ SDT <- function(criterion, signal_mean, noise_mean, input_sigma = 1, likelihood_
   
   #draw the foil likelihood line
   scalar = .1
-  location_foil_like = 12
+  location_foil_like = signal_mean + input_sigma + 6
   segments(criterion,foil_like, location_foil_like, foil_like, lty=3) 
   segments(location_foil_like,0, location_foil_like,foil_like, col = line_col) #actual foil likelihood line
   text(location_foil_like + .5,foil_like-foil_like/3,"foil likelihood", srt = 90) #srt is to turn the text vertical 
@@ -54,20 +54,20 @@ SDT <- function(criterion, signal_mean, noise_mean, input_sigma = 1, likelihood_
   segments(location_foil_like- scalar, 0, location_foil_like+ scalar, 0, col = line_col) #the end bits
   
   #draw the target likelihood line
-  location_target_like = 11
+  location_target_like = signal_mean + input_sigma + 4
   segments(criterion,target_like, location_target_like, target_like, lty=3)
   segments(location_target_like,0,location_target_like,target_like, col = line_col) #actual target likelihood line
   text(location_target_like + .5,target_like-target_like/3,"target likelihood", srt = 90)
   segments(location_target_like - scalar, target_like, location_target_like+scalar, target_like, col = line_col)
   segments(location_target_like - scalar, 0, location_target_like+scalar, 0, col = line_col) #the end bits
   
-  
 }
 
-criterion = 4.2
+criterion = 4
 signal_mean = 6
 noise_mean = 4
 #input_sigma = 1 #default at 1
+#color arguments defaulted at blue, red, black
 
 SDT(criterion, signal_mean, noise_mean)
 
@@ -103,6 +103,8 @@ SDT(criterion, signal_mean, noise_mean)
 # 
 # criterion, signal_mean, noise_mean are necessary, sigma (standard deviation) is set at 1.
 # colors are at the moment hard coded in the function, criterion usually lies between signal and noise mean.
+# function only works for positive values of signal and noise, noiseis to be expected to be smaller then the signal, 
+# function plots from zero up to signal_mean + 4
 # 
 # source
 # 
